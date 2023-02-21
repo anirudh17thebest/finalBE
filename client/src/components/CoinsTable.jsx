ae6259b8bad16ee0ef7d68dcd630e13ff2714048
Anirudh Prajapati <87312208+anirudh17thebest@users.noreply.github.com>
2023-02-22 00:07:59 +0530

    Update CoinsTable.jsx

--- client/src/components/CoinsTable.jsx
+++ client/src/components/CoinsTable.jsx
@@ -128,7 +128,7 @@ export default function CoinsTable() {
                     const profit = row.price_change_percentage_24h > 0;
                     return (
                       <TableRow
-                        onClick={() => navigate(`/market/coin/${row.id}`)}
+                        onClick={() => navigate(`/tracker/coin/${row.id}`)}
                         className={classes.row}
                         key={row.name}
                       >
